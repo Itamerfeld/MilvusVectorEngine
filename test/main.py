dea96ab45d6e3bef332fa8f8d113c3922b857337
korenh <korenx123@gmail.com>
2021-03-15 21:20:38 +0200

    conf

--- test/main.py
+++ test/main.py
@@ -5,22 +5,32 @@ from PIL import Image
 
 #  CD root Folder & Run "nosetests --verbosity=2 main"
 
+def config():
+    return{
+        "API_CREATE": "http://localhost:5000/create_collection/",
+        "API_INFO": "http://localhost:5000/info_collection/",
+        "API_DROP": "http://localhost:5000/drop_collection/",
+        "API_KEY": "?key=secret"
+    }
+
+
 collection_name = 'test'
+config = config()
 
 
 def test_create():
     response = requests.get(
-        'http://localhost:5000/create_collection/'+collection_name+'?key=secret')
+        config['API_CREATE']+collection_name + config['API_KEY'])
     assert_true(response.ok)
 
 
 def test_info():
     response = requests.get(
-        'http://localhost:5000/info_collection/'+collection_name+'?key=secret')
+        config['API_INFO']+collection_name + config['API_KEY'])
     assert_true(response.ok)
 
 
 def test_drop():
     response = requests.get(
-        'http://localhost:5000/drop_collection/'+collection_name+'?key=secret')
+        config['API_DROP']+collection_name + config['API_KEY'])
     assert_true(response.ok)
